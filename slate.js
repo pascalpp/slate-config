205cc104dddae0073194b4b94450d9d439659b84
Pascal <pascal@pascal.com>
2023-05-23 09:53:25 -0400

    var -> const

--- slate.js
+++ slate.js
@@ -15,91 +15,96 @@ log('==== loaded ==========================================================')
 var sizes = [];
 
 slate.eachScreen(screen => {
-  var rect = screen.rect()
+  const rect = screen.rect()
   sizes.push(rect.width)
   sizes.sort((a,b) => b - a)
 })
 
-// var relaunch = slate.operation('relaunch')
+// const relaunch = slate.operation('relaunch')
 // slate.bind('pad0:ctrl,alt,shift', relaunch)
 // slate.bind('0:ctrl,alt,shift', relaunch)
 
-var xPercent30 = 'screenSizeX*3/10'
-var xPercent40 = 'screenSizeX*4/10'
-var xPercent50 = 'screenSizeX*5/10'
-var xPercent60 = 'screenSizeX*6/10'
-var xPercent70 = 'screenSizeX*7/10'
-var xPercent80 = 'screenSizeX*8/10'
-var xPercent90 = 'screenSizeX*9/10'
-var xPercent95 = 'screenSizeX*95/100'
-var xPercent100 = 'screenSizeX'
+const xPercent30 = 'screenSizeX*3/10'
+const xPercent40 = 'screenSizeX*4/10'
+const xPercent50 = 'screenSizeX*5/10'
+const xPercent60 = 'screenSizeX*6/10'
+const xPercent70 = 'screenSizeX*7/10'
+const xPercent80 = 'screenSizeX*8/10'
+const xPercent90 = 'screenSizeX*9/10'
+const xPercent95 = 'screenSizeX*95/100'
+const xPercent100 = 'screenSizeX'
 
-var yPercent30 = 'screenSizeY*3/10'
-var yPercent40 = 'screenSizeY*4/10'
-var yPercent50 = 'screenSizeY*5/10'
-var yPercent60 = 'screenSizeY*6/10'
-var yPercent70 = 'screenSizeY*7/10'
-var yPercent80 = 'screenSizeY*8/10'
-var yPercent90 = 'screenSizeY*9/10'
-var yPercent100 = 'screenSizeY'
+const yPercent30 = 'screenSizeY*3/10'
+const yPercent40 = 'screenSizeY*4/10'
+const yPercent50 = 'screenSizeY*5/10'
+const yPercent60 = 'screenSizeY*6/10'
+const yPercent70 = 'screenSizeY*7/10'
+const yPercent80 = 'screenSizeY*8/10'
+const yPercent90 = 'screenSizeY*9/10'
+const yPercent100 = 'screenSizeY'
 
 // width varants
-var leftWidth = slate.operation('move', {
+const leftWidth = slate.operation('move', {
   x: 'screenOriginX',
   y: 'windowTopLeftY',
   width: 100,
   height: 'windowSizeY',
 })
-var rightWidth = slate.operation('move', {
+const rightWidth = slate.operation('move', {
   x: 'screenOriginX+screenSizeX-100',
   y: 'windowTopLeftY',
   width: 100,
   height: 'windowSizeY',
 })
 
-var leftWidth30 = leftWidth.dup({ width: xPercent30 })
-var leftWidth40 = leftWidth.dup({ width: xPercent40 })
-var leftWidth50 = leftWidth.dup({ width: xPercent50 })
-var leftWidth60 = leftWidth.dup({ width: xPercent60 })
-var leftWidth70 = leftWidth.dup({ width: xPercent70 })
-var leftWidth80 = leftWidth.dup({ width: xPercent80 })
-var leftWidth90 = leftWidth.dup({ width: xPercent90 })
-var leftWidth100 = leftWidth.dup({ width: xPercent100 })
+const leftWidth30 = leftWidth.dup({ width: xPercent30 })
+const leftWidth40 = leftWidth.dup({ width: xPercent40 })
+const leftWidth50 = leftWidth.dup({ width: xPercent50 })
+const leftWidth60 = leftWidth.dup({ width: xPercent60 })
+const leftWidth70 = leftWidth.dup({ width: xPercent70 })
+const leftWidth80 = leftWidth.dup({ width: xPercent80 })
+const leftWidth90 = leftWidth.dup({ width: xPercent90 })
+const leftWidth95 = leftWidth.dup({ width: xPercent95 })
+const leftWidth100 = leftWidth.dup({ width: xPercent100 })
 
-var rightWidth30 = rightWidth.dup({
+const rightWidth30 = rightWidth.dup({
   width: xPercent30,
   x: 'screenOriginX+screenSizeX-' + xPercent30,
 })
-var rightWidth40 = rightWidth.dup({
+const rightWidth40 = rightWidth.dup({
   width: xPercent40,
   x: 'screenOriginX+screenSizeX-' + xPercent40,
 })
-var rightWidth50 = rightWidth.dup({
+const rightWidth50 = rightWidth.dup({
   width: xPercent50,
   x: 'screenOriginX+screenSizeX-' + xPercent50,
 })
-var rightWidth60 = rightWidth.dup({
+const rightWidth60 = rightWidth.dup({
   width: xPercent60,
   x: 'screenOriginX+screenSizeX-' + xPercent60,
 })
-var rightWidth70 = rightWidth.dup({
+const rightWidth70 = rightWidth.dup({
   width: xPercent70,
   x: 'screenOriginX+screenSizeX-' + xPercent70,
 })
-var rightWidth80 = rightWidth.dup({
+const rightWidth80 = rightWidth.dup({
   width: xPercent80,
   x: 'screenOriginX+screenSizeX-' + xPercent80,
 })
-var rightWidth90 = rightWidth.dup({
+const rightWidth90 = rightWidth.dup({
   width: xPercent90,
   x: 'screenOriginX+screenSizeX-' + xPercent90,
 })
-var rightWidth100 = rightWidth.dup({
+const rightWidth95 = rightWidth.dup({
+  width: xPercent95,
+  x: 'screenOriginX+screenSizeX-' + xPercent95,
+})
+const rightWidth100 = rightWidth.dup({
   width: xPercent100,
   x: 'screenOriginX+screenSizeX-' + xPercent100,
 })
 
-var sizeLeft = slate.operation('chain', {
+const sizeLeft = slate.operation('chain', {
   operations: [
     leftWidth30,
     leftWidth40,
@@ -108,10 +113,11 @@ var sizeLeft = slate.operation('chain', {
     leftWidth70,
     leftWidth80,
     leftWidth90,
+    leftWidth95,
     leftWidth100,
   ],
 })
-var sizeRight = slate.operation('chain', {
+const sizeRight = slate.operation('chain', {
   operations: [
     rightWidth30,
     rightWidth40,
@@ -120,6 +126,7 @@ var sizeRight = slate.operation('chain', {
     rightWidth70,
     rightWidth80,
     rightWidth90,
+    rightWidth95,
     rightWidth100,
   ],
 })
@@ -129,62 +136,62 @@ slate.bind('2:ctrl', sizeRight)
 slate.bind('pad1:ctrl', sizeLeft)
 slate.bind('pad2:ctrl', sizeRight)
 
-var topHeight = slate.operation('move', {
+const topHeight = slate.operation('move', {
   x: 'windowTopLeftX',
   y: 'screenOriginY',
   width: 'windowSizeX',
   height: 100,
 })
-var bottomHeight = slate.operation('move', {
+const bottomHeight = slate.operation('move', {
   x: 'windowTopLeftX',
   y: 'screenOriginY+screenSizeY-100',
   width: 'windowSizeX',
   height: 100,
 })
 
-var topHeight30 = topHeight.dup({ height: yPercent30 })
-var topHeight40 = topHeight.dup({ height: yPercent40 })
-var topHeight50 = topHeight.dup({ height: yPercent50 })
-var topHeight60 = topHeight.dup({ height: yPercent60 })
-var topHeight70 = topHeight.dup({ height: yPercent70 })
-var topHeight80 = topHeight.dup({ height: yPercent80 })
-var topHeight90 = topHeight.dup({ height: yPercent90 })
-var topHeight100 = topHeight.dup({ height: yPercent100 })
+const topHeight30 = topHeight.dup({ height: yPercent30 })
+const topHeight40 = topHeight.dup({ height: yPercent40 })
+const topHeight50 = topHeight.dup({ height: yPercent50 })
+const topHeight60 = topHeight.dup({ height: yPercent60 })
+const topHeight70 = topHeight.dup({ height: yPercent70 })
+const topHeight80 = topHeight.dup({ height: yPercent80 })
+const topHeight90 = topHeight.dup({ height: yPercent90 })
+const topHeight100 = topHeight.dup({ height: yPercent100 })
 
-var bottomHeight30 = bottomHeight.dup({
+const bottomHeight30 = bottomHeight.dup({
   height: yPercent30,
   y: 'screenOriginY+screenSizeY-' + yPercent30,
 })
-var bottomHeight40 = bottomHeight.dup({
+const bottomHeight40 = bottomHeight.dup({
   height: yPercent40,
   y: 'screenOriginY+screenSizeY-' + yPercent40,
 })
-var bottomHeight50 = bottomHeight.dup({
+const bottomHeight50 = bottomHeight.dup({
   height: yPercent50,
   y: 'screenOriginY+screenSizeY-' + yPercent50,
 })
-var bottomHeight60 = bottomHeight.dup({
+const bottomHeight60 = bottomHeight.dup({
   height: yPercent60,
   y: 'screenOriginY+screenSizeY-' + yPercent60,
 })
-var bottomHeight70 = bottomHeight.dup({
+const bottomHeight70 = bottomHeight.dup({
   height: yPercent70,
   y: 'screenOriginY+screenSizeY-' + yPercent70,
 })
-var bottomHeight80 = bottomHeight.dup({
+const bottomHeight80 = bottomHeight.dup({
   height: yPercent80,
   y: 'screenOriginY+screenSizeY-' + yPercent80,
 })
-var bottomHeight90 = bottomHeight.dup({
+const bottomHeight90 = bottomHeight.dup({
   height: yPercent90,
   y: 'screenOriginY+screenSizeY-' + yPercent90,
 })
-var bottomHeight100 = bottomHeight.dup({
+const bottomHeight100 = bottomHeight.dup({
   height: yPercent100,
   y: 'screenOriginY+screenSizeY-' + yPercent100,
 })
 
-var sizeTop = slate.operation('chain', {
+const sizeTop = slate.operation('chain', {
   operations: [
     topHeight30,
     topHeight40,
@@ -196,7 +203,7 @@ var sizeTop = slate.operation('chain', {
     topHeight100,
   ],
 })
-var sizeBottom = slate.operation('chain', {
+const sizeBottom = slate.operation('chain', {
   operations: [
     bottomHeight30,
     bottomHeight40,
@@ -214,7 +221,7 @@ slate.bind('pad4:ctrl', sizeBottom)
 slate.bind('7:ctrl', sizeTop)
 slate.bind('4:ctrl', sizeBottom)
 
-var taller = slate.operation('move', {
+const taller = slate.operation('move', {
   x: 'windowTopLeftX',
   y: 'windowTopLeftY',
   width: 'windowSizeX',
@@ -222,7 +229,7 @@ var taller = slate.operation('move', {
 })
 slate.bind('down:ctrl,alt', taller)
 
-var shorter = slate.operation('move', {
+const shorter = slate.operation('move', {
   x: 'windowTopLeftX',
   y: 'windowTopLeftY',
   width: 'windowSizeX',
@@ -230,7 +237,7 @@ var shorter = slate.operation('move', {
 })
 slate.bind('up:ctrl,alt', shorter)
 
-var wider = slate.operation('move', {
+const wider = slate.operation('move', {
   x: 'windowTopLeftX',
   y: 'windowTopLeftY',
   width: 'windowSizeX+20',
@@ -238,7 +245,7 @@ var wider = slate.operation('move', {
 })
 slate.bind('right:ctrl,alt', wider)
 
-var narrower = slate.operation('move', {
+const narrower = slate.operation('move', {
   x: 'windowTopLeftX',
   y: 'windowTopLeftY',
   width: 'windowSizeX-20',
@@ -246,12 +253,12 @@ var narrower = slate.operation('move', {
 })
 slate.bind('left:ctrl,alt', narrower)
 
-var onethird = function(win) {
+const onethird = function(win) {
   if (!win) return
-  var w = win.rect()
-  var screen = slate.screen()
-  var s = screen.rect()
-  var targetwidth = (s.width * 4) / 10
+  const w = win.rect()
+  const screen = slate.screen()
+  const s = screen.rect()
+  const targetwidth = (s.width * 4) / 10
   if (w.x === s.x && Math.abs(w.width - targetwidth) < 10) {
     win.doOperation(rightWidth40)
   } else if (w.x + w.width === s.width && Math.abs(w.width - targetwidth) > 10) {
@@ -262,12 +269,12 @@ var onethird = function(win) {
 }
 slate.bind('pad1:cmd', onethird)
 
-var half = function(win) {
+const half = function(win) {
   if (!win) return
-  var w = win.rect()
-  var screen = slate.screen()
-  var s = screen.rect()
-  var targetwidth = (s.width * 5) / 10
+  const w = win.rect()
+  const screen = slate.screen()
+  const s = screen.rect()
+  const targetwidth = (s.width * 5) / 10
   if (w.x === s.x && Math.abs(w.width - targetwidth) < 10) {
     win.doOperation(rightWidth50)
   } else if (w.x + w.width === s.width && Math.abs(w.width - targetwidth) > 10) {
@@ -278,12 +285,12 @@ var half = function(win) {
 }
 slate.bind('pad2:cmd', half)
 
-var twothirds = function(win) {
+const twothirds = function(win) {
   if (!win) return
-  var w = win.rect()
-  var screen = slate.screen()
-  var s = screen.rect()
-  var targetwidth = (s.width * 6) / 10
+  const w = win.rect()
+  const screen = slate.screen()
+  const s = screen.rect()
+  const targetwidth = (s.width * 6) / 10
   if (w.x === s.x && Math.abs(w.width - targetwidth) < 10) {
     win.doOperation(rightWidth60)
   } else if (w.x + w.width === s.width && Math.abs(w.width - targetwidth) > 10) {
@@ -303,7 +310,7 @@ function getNextScreen(screen) {
 function defaultWindowSize(win, screen) {
   screen = screen || win && win.screen()
   if (!screen) return
-  var rect = screen.rect()
+  const rect = screen.rect()
   let move = slate.operation('move', rect, screen)
   if (isBigScreen(screen)) {
     move = defaultWindowSizeForBigScreen({ move, win, screen })
@@ -317,7 +324,7 @@ slate.bind('w:ctrl', defaultWindowSize)
 function fillScreen(win, screen) {
   screen = screen || win && win.screen()
   if (!screen) return
-  var rect = screen.rect()
+  const rect = screen.rect()
   const move = slate.operation('move', rect, screen)
   win.doOperation(move)
 }
@@ -325,7 +332,7 @@ slate.bind('w:ctrl,shift', fillScreen)
 
 
 function moveToOtherScreen(win) {
-  var screen = getNextScreen(win.screen())
+  const screen = getNextScreen(win.screen())
   defaultWindowSize(win, screen)
 }
 slate.bind('2:ctrl,alt', moveToOtherScreen)
@@ -333,13 +340,13 @@ slate.bind('pad2:ctrl,alt', moveToOtherScreen)
 
 function defaultWindowSizeForBigScreen({ move, win, screen }) {
   if (!win) return
-  var appName = win.app().name()
-  var title = win.title()
+  const appName = win.app().name()
+  const title = win.title()
   switch (appName) {
     case 'Code':
       move = move.dup({
-        width: 'screenSizeX*7/10',
         x: 'screenOriginX+screenSizeX-screenSizeX*7/10',
+        width: 'screenSizeX*7/10',
         screen,
       })
       break
@@ -464,7 +471,7 @@ function defaultWindowSizeForBigScreen({ move, win, screen }) {
 }
 
 function defaultWindowSizeForSmallScreen({ move, win, screen }) {
-  var appName = win.app().name()
+  const appName = win.app().name()
   switch (appName) {
     case 'Messages':
       move = move.dup({
@@ -505,7 +512,7 @@ function isBigScreen(screen) {
 }
 
 function log(...args) {
-  var msg = args.map(formatArg).join(' ')
+  const msg = args.map(formatArg).join(' ')
   slate.log('slatelog', msg)
 }
 
